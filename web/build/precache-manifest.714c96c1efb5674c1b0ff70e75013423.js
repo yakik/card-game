2eef2cd882bf44ee8dde0716a2c170b836ebbb11
yakik <yaki.koren@gmail.com>
2020-04-09 00:13:12 +0300

    fixed heroku-deploy

---
+++ web/build/precache-manifest.714c96c1efb5674c1b0ff70e75013423.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79a75e89ff60c3ed93f72954d09e360f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06b537d0b86b362d0b0f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bf4ebd9977b4f0451ee6",
+    "url": "/static/js/2.99cab11e.chunk.js"
+  },
+  {
+    "revision": "f65b45f562155f0fa2653b346e3552a6",
+    "url": "/static/js/2.99cab11e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06b537d0b86b362d0b0f",
+    "url": "/static/js/main.dbf4f68b.chunk.js"
+  },
+  {
+    "revision": "65762bcbee126357b070",
+    "url": "/static/js/runtime-main.35eb2b37.js"
+  }
+]);
