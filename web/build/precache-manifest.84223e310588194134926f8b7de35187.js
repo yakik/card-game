7e8a5991eb4500cbfa8f24f196a1c8979d477459
yakik <yaki.koren@gmail.com>
2020-04-09 00:00:09 +0300

    removed build from git ignore

---
+++ web/build/precache-manifest.84223e310588194134926f8b7de35187.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f22b77b0633800d23df7eb4d2b7ddc4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f93efd59d94231a3e636",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "225581796fc7d8cc75f5",
+    "url": "/static/js/2.1fb61026.chunk.js"
+  },
+  {
+    "revision": "f65b45f562155f0fa2653b346e3552a6",
+    "url": "/static/js/2.1fb61026.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f93efd59d94231a3e636",
+    "url": "/static/js/main.bfe472fc.chunk.js"
+  },
+  {
+    "revision": "65762bcbee126357b070",
+    "url": "/static/js/runtime-main.35eb2b37.js"
+  }
+]);
