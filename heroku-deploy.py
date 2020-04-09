0b9669bf6834c73660cb95e01e0434c53801a945
yakik <yaki.koren@gmail.com>
2020-04-09 08:11:42 +0300

    deployfile

--- heroku-deploy.py
+++ heroku-deploy.py
@@ -6,14 +6,7 @@ FILE_PATH = os.path.dirname(os.path.realpath(__file__))
 
 
 def init_deploy():
-    shutil.rmtree(os.path.join(FILE_PATH, 'web'))
-
-
-def copy_backend():
-    shutil.copytree(
-        os.path.join(FILE_PATH, 'backend'),
-        os.path.join(FILE_PATH, 'web'),
-        ignore=shutil.ignore_patterns('node_modules', '.env', 'db.json', '.eslintrc.js', 'tests'))
+    shutil.rmtree(os.path.join(FILE_PATH, 'backend', 'web'))
 
 
 def build_frontend():
@@ -23,24 +16,12 @@ def build_frontend():
 def copy_build_folder():
     shutil.copytree(
         os.path.join(FILE_PATH, 'frontend', 'build'),
-        os.path.join(FILE_PATH, 'web', 'build'))
-   # shutil.rmtree(os.path.join(FILE_PATH, 'frontend', 'build'))
-
-
-def push_to_heroku():
-    os.system('git add .')
-    os.system('git commit -m "Commit from heroku-deploy.py for deployment"')
-    os.system('git push origin master')
-    os.system('git subtree push --prefix web heroku master')
-
+        os.path.join(FILE_PATH, 'backend', 'build'))
 
 def main():
     init_deploy()
-    copy_backend()
     build_frontend()
     copy_build_folder()
-    push_to_heroku()
-
 
 if __name__ == "__main__":
     main()
