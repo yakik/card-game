adc27dcb0c8b11acaaadacfa50ef786ec425931f
yakik <yaki.koren@gmail.com>
2020-04-13 10:08:37 +0300

    added tests, not passing

--- backend/__tests__/engine.integration.tests.js
+++ backend/__tests__/engine.integration.tests.js
@@ -2,12 +2,56 @@ import {
 
 } from "../modules/pack";
 
+
+function whichPileToAdd(piles,card){
+    return -2
+} 
+
+
+
 describe("validation tests", () => {
 
 
-    test("get possible solutions", () => {
+    test.skip("between", () => {
 
-        expect(1).toBe(1)
+        let piles = [[{number:4},{number:16},{number:27}],
+        [{number:18}],
+        [{number:2},{number:17},{number:35},{number:56},{number:90}],
+        [{number:30},{number:45},{number:56}]]
+
+        let card = {number:19}
+
+        expect(whichPileToAdd(piles,card)).toBe(1)
+
+        
+    });
+
+    test.skip("largest", () => {
+
+        let piles = [[{number:4},{number:16},{number:27}],
+        [{number:18}],
+        [{number:2},{number:17},{number:35},{number:56},{number:90}],
+        [{number:30},{number:45},{number:56}]]
+
+        let card = {number:92}
+
+        expect(whichPileToAdd(piles,card)).toBe(2)
+
+        
+    });
+
+    test.skip("smaller", () => {
+
+        let piles = [[{number:4},{number:16},{number:27}],
+        [{number:18}],
+        [{number:2},{number:17},{number:35},{number:56},{number:90}],
+        [{number:30},{number:45},{number:56}]]
+
+        let card = {number:1}
+
+        expect(whichPileToAdd(piles,card)).toBe(-1)
+
+        
     });
 
 })
